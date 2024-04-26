4c49190c13b47016de38ecdeffcdfbb682e814c1
Atul Kumar <117622806+Atulkumar3006@users.noreply.github.com>
2024-04-26 22:22:04 +0530

    Add files via upload

---
+++ slider-data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'WI  vs IND 317/8 (50)',
+        des: 'West Indies posted a formidable total of 317 runs for the loss of 8 wickets in their allotted 50 overs against India. This suggests a competitive batting performance from the West Indian team.',
+        image: 'slider 2.PNG'
+    },
+    {
+        name: 'Rudra: The Edge of Darkness',
+        des: ' The movie revolves around a police officer named Rudra, played by Ajay Devgn, who finds himself caught in a dangerous cat-and-mouse game with a notorious criminal. ',
+        image: 'slider 1.PNG'
+    },
+    {
+        name: 'The Book of Boba Fett',
+        des: ' The series picks up after the events of "The Mandalorian" and focuses on Boba Fetts  endeavors to establish his authority over the criminal underworld of Tatooine. ',
+        image: 'slider 3.PNG'
+    },
+    {
+        name: 'Raya and the last dragon',
+        des: 'Raya is determined to locate Sisu, who is believed to possess the power to banish the Druun. Along the way, Raya forms unexpected alliances with colorful characters, including a con baby named Tuk Tuk, a skilled but eccentric warrior named Tong, and a young entrepreneur named Boun.',
+        image: 'slider 4.PNG'
+    },
+    { 
+        name: 'Luca',
+        des: 'As Luca and Alberto befriend a spirited human girl named Giulia, they become involved in a local competition involving swimming and pasta-making. Through their experiences, Luca learns about friendship, courage, and the importance of being true to oneself.',
+        image: 'slider 5.PNG'
+    }
+]
